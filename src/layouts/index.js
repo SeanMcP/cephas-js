4b4eb68fd11168bc0a01cd43c5725740217d297a
Sean McPherson <seanmcp25@gmail.com>
2018-09-03 07:34:13 -0400

    Add cephas-type and md-colors CSS
    Removed default Gatsby styles.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet'
 
 import Block from '../components/Block'
 import Header from '../components/header'
-import './index.css'
+import '../styles/cephas-type.css'
 import './cephas-layout.css'
 
 const Layout = ({ children, data }) => (
@@ -18,18 +18,8 @@ const Layout = ({ children, data }) => (
     />
     {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
     <Block>
-        <div>Test</div>
-    </Block>
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
       {children()}
-    </div>
+    </Block>
   </div>
 )
 
