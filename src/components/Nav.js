cf86dc985de522d0239743383e50edb5a099a04e
Sean McPherson <seanmcp25@gmail.com>
2018-09-04 06:51:42 -0400

    Create Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import PropTypes from 'prop-types'
+
+const Nav = (props) => (
+    <nav className={`cephas Nav ${props.className ? props.className : ''}`}>
+        {props.links.map(
+            (link, i) => (
+                <Link
+                    key={i}
+                    to={link.href}
+                >
+                    {link.text}
+                </Link>
+            )
+        )}
+    </nav>
+)
+
+Nav.propTypes = {
+    className: PropTypes.string,
+    links: PropTypes.arrayOf(
+        PropTypes.shape({
+            href: PropTypes.string.isRequired,
+            text: PropTypes.string.isRequired
+        })
+    )
+}
+
+export default Nav
