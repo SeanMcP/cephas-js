18ce13871477de587ec0cd9eb21f961da778e323
Sean McPherson <seanmcp25@gmail.com>
2018-09-06 06:36:05 -0400

    Update pages with semantic HTML

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const SecondPage = () => (
-  <div>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </div>
+    <main>
+        <h1>Hi from the second page</h1>
+        <p>Welcome to page 2</p>
+        <Link to="/">Go back to the homepage</Link>
+    </main>
 )
 
 export default SecondPage
